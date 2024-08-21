4fab477deb10237e6a0e98f83fbe8b5b7ddea2a2
CGBA <minkyoo2@naver.com>
2024-08-21 20:47:46 +0900

    fix minor settings

--- Model_Training/run_clusters.sh
+++ Model_Training/run_clusters.sh
@@ -3,6 +3,11 @@
 # Define the list of cluster numbers
 c_list=(33 136 48 281 170 319 217 5 8 10 93 68 9 124 69 74 125 53 13 473 27 112 100 159 24 128 318 39 73 77 330 221 414 138 156 411 25 450 94 856 0 14 459 97 536 42 45 15 75 214 167 180 23 131)
 
+# Check if TrainLogs directory exists, if not create it
+if [ ! -d "TrainLogs" ]; then
+    mkdir TrainLogs
+fi
+
 # Iterate through each cluster number
 for cluster in "${c_list[@]}"; do
    # Run the python script with nohup
